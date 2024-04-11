a03da4a0fe42c2737b9dd621ea5148955d2cf71f
mariahlins <136923303+mariahlins@users.noreply.github.com>
2024-04-11 20:16:32 -0300

    Update loteRoutes.js

--- src/routes/loteRoutes.js
+++ src/routes/loteRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const LoteController = require('.\controllers\LoteController.js');
+const LoteController = require('../controllers/LoteController.js');
 
 const loteController = new LoteController();
 const router = Router();
@@ -11,17 +11,3 @@ router.put('/lote/:id',(req,res)=>{loteController.atualiza(req,res);});
 router.delete('/lote/:id',(req,res)=>{loteController.deleta(req,res);});
 
 module.exports = router;
-
-
-/*const {Router} = require('express');
-const LoteController = require('../controllers/LoteController.js');
-
-const routes = Router();
-
-routes.get('/lotes',LoteController.getLotes);
-routes.get('./lote/:id',LoteController.getLotesByPK);
-routes.put('./lotes/:id',LoteController.updateLote);
-routes.post('./lotes',LoteController.createLote);
-routes.delete('./lotes/:id',LoteController.deleteLote);
-
-modules.exports = routes;*/
